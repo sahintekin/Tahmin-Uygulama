bbb42313c0a24ec902482aa6fe2d6c13786a9671
sahintekin <ahmetsahintekin1@gmail.com>
2024-07-22 23:44:21 +0300

    Tahmin Uygulama
    Sayı Tahmin Etme Consol Uygulaması

---
+++ NumaraOyunu/NumaraOyunu/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumaraOyunu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                NubmerGame();
+
+            }
+            catch (Exception)
+            {
+                NubmerGame();
+              
+            }
+    
+            Console.ReadLine();
+        }
+        static void NubmerGame()
+        {
+            int number = new Random().Next(1, 51);
+
+            int KalanHak = 10;
+            Console.WriteLine("İsminiz");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Tahminizini giriniz:");
+            int userNumber = int.Parse(Console.ReadLine());
+
+            while (userNumber != number)
+            {
+                KalanHak--;
+                if (Math.Abs(number - userNumber) < 5)
+                {
+                    Console.WriteLine("Sıcak");
+                }
+                else
+                {
+                    Console.WriteLine("Soğuk");
+
+                }
+                userNumber = int.Parse(Console.ReadLine());
+                if (KalanHak == 0)
+                {
+                    Console.WriteLine("Hakkınız bitti :(" + name + ".Mallesef bulamadınız. Rakam: " + number);
+                    break;
+                }
+            }
+            if (userNumber == number)
+            {
+                Console.WriteLine("Tebrikler");
+            }
+        }
+    }
+}
